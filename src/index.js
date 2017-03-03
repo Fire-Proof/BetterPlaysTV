1bc72476009647a00c1f477f9bfc183cf48a8007
Nickolaj Jepsen <nickolaj1177@gmail.com>
2017-03-03 14:09:51 +0100

    added eslint

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-'use strict';
-import home from './routes/home.js';
-import general from './routes/general.js';
-import style from '../style/index.scss';
+import home from './routes/home';
+import general from './routes/general';
+import '../style/index.scss';
 // var $ = require('jquery');
 
 // alert('Test');
 const routes = {
-    "http://plays.tv/home": home
-}
+    'http://plays.tv/home': home,
+};
 
 
-function router(){
-    if (window.location.href in routes){
+function router() {
+    if (window.location.href in routes) {
         routes[window.location.href]();
     }
     general();
--- src/routes/general.js
+++ src/routes/general.js
@@ -1,36 +1,32 @@
-import utils from "../utils.js"
+import utils from '../utils';
 
 const DisableAutoReplay = class DisableAutoReplay extends utils.InfLoadWatch {
-    constructor(){
-        super(".mod-vop-feed, .mod-feed")
+    constructor() {
+        super('.mod-vop-feed, .mod-feed');
 
-        this.feedAction(document.querySelectorAll(".mod-vop-feed, .feed-item"));
+        this.feedAction(document.querySelectorAll('.mod-vop-feed, .feed-item'));
     }
 
     feedAction(feedItems) {
-        console.log(feedItems)
-        for (let feedItem of feedItems){
-            let videos = feedItem.querySelectorAll(".ui-player video")
-            console.log(videos)
-            for (let video of videos){
+        for (const feedItem of feedItems) {
+            const videos = feedItem.querySelectorAll('.ui-player video')
+            for (const video of videos) {
                 video.loop = false;
             }
         }
-    };
-
+    }
 };
 
 const disableSocialHovers = function disableSocialHovers() {
-    require("../../style/disableSocialHovers.scss");
+    require('../../style/disableSocialHovers.scss');
 };
 
-const expandedView = function expandedView(){
-    require("../../style/expandedView.scss");
-}
+const expandedView = function expandedView() {
+    require('../../style/expandedView.scss');
+};
 
-
-export default function(){
+export default function () {
     new DisableAutoReplay();
     disableSocialHovers();
     expandedView();
-};
+}
